a987d527b7e7a3b7da572212a11342741eb7986d
Edward Han <edwrdhn@gmail.com>
2024-07-27 14:41:55 -0400

    Initial Commit

---
+++ LocalLink/LocalLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalLinkApp.swift
+//  LocalLink
+//
+//  Created by Edward Han on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
